368b467980ef8bc2f8fb606ce17d02d18f715280
PatinesLuis <luispatines17@gmail.com>
2025-03-21 03:00:15 -0300

    Preparando objeto User

---
+++ dao/userDAO.php
@@ -0,0 +1,66 @@
+<?php
+
+include_once("models/user.php");
+
+class userDAO implements userDAOInterface{
+
+    private $conn;
+    private $url;
+
+    public function __construct(PDO $conn, $url){
+        $this->conn = $conn;
+        $this->url = $url;
+    }
+
+    public function builderUser($data){
+
+        $user = new User();
+
+        $user->id = $data["id"];
+        $user->name = $data["name"];
+        $user->lastname = $data["lastname"];
+        $user->email = $data["email"];
+        $user->password = $data["password"];
+        $user->image = $data["image"];
+        $user->bio = $data["bio"];
+        $user->token = $data["token"];
+
+        return $user;
+    }
+
+    public function create(User $user, $authUser = false){
+
+    }
+
+    public function update (User $user){
+
+    }
+
+    public function  verifyToken($protected = false){
+
+    }
+
+    public function setTokenToSession($token, $redirect = true){
+
+    }
+
+    public function authenticateUser($email, $password){
+
+    }
+
+    public function findByEmail($email){
+
+    }
+
+    public function findById($id){
+
+    }
+
+    public function findByToken($token){
+
+    }
+
+    public function changePassword(User $user){
+
+    }
+}
